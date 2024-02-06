68d1f75662f3b83e0145aa7aa3920e66fafd2ba5
kelsy gagnebin <kelsymichael@users.noreply.github.com>
2024-02-06 14:37:23 -0700

    finally got eleventy-img semi-working

--- .eleventy.js
+++ .eleventy.js
@@ -3,20 +3,42 @@ const pluginRss = require('@11ty/eleventy-plugin-rss');
 const dateFilter = require('./src/filters/date-filter.js');
 const w3cDateFilter = require('./src/filters/w3c-date-filter.js');
 const navLinks = require('./src/filters/navLinks.js');
+const Image = require("@11ty/eleventy-img");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
-
   eleventyConfig.addLayoutAlias('main', 'layouts/main.njk');
-
   eleventyConfig.addPassthroughCopy('src/images');
-
   eleventyConfig.addFilter('dateFilter', dateFilter);
   eleventyConfig.addFilter('w3cDateFilter', w3cDateFilter);
   eleventyConfig.addFilter('navLinks', navLinks);
 
+  // Rename the shortcode to "responsiveImage"
+  eleventyConfig.addNunjucksAsyncShortcode("responsiveImage", async (src, alt) => {
+    let options = {
+      widths: [320, 640, 768, 992, 1200, null], // Use the original image width
+      formats: ["webp", "jpeg"], // Specify formats
+      urlPath: "/images/",
+      outputDir: "./_site/images/",
+      // ... other options
+    };
+
+    // Generate images
+    let metadata = await Image(`./src/${src}`, options);
+
+    let imageAttributes = {
+      alt,
+      sizes: "(min-width: 1200px) 1200px, (min-width: 992px) 992px, (min-width: 768px) 768px, 100vw",
+      loading: "lazy",
+      decoding: "async",
+    };
+
+    // Return the image HTML
+    return Image.generateHTML(metadata, imageAttributes);
+  });
+
   eleventyConfig.addTransform('htmlmin', function(content, outputPath) {
-    if( outputPath.endsWith('.html') ) {
+    if (outputPath.endsWith('.html')) {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
@@ -24,7 +46,6 @@ module.exports = function (eleventyConfig) {
       });
       return minified;
     }
-
     return content;
   });
 
